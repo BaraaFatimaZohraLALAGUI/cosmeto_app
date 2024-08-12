65a147ed0a442ddaabfdbe5cc7cc77b029994b14
BaraaFatimaZohraLALAGUI <lalaguibaraa@gmail.com>
2024-08-12 11:26:53 +0000

    adding embeddings get

--- ui.py
+++ ui.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pickle 
-
+import pandas as pd
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+import torch
+import re
 
 class UI:
     def __init__(self):
@@ -8,6 +12,11 @@ class UI:
         if "data" not in st.session_state:
             st.session_state.data = []
 
+        # Load the tokenizer and model 
+        self.tokenizer_pubmedbert = AutoTokenizer.from_pretrained("microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract")
+        self.model_pubmedbert = AutoModel.from_pretrained("microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract")
+
+        # Load SVM classifier
         with open('svc.pkl', 'rb') as file:
             self.svc_model = pickle.load(file)
 
@@ -32,9 +41,20 @@ class UI:
             compounds_sentence = ', '.join(st.session_state.data)
             st.write(f"Compounds list: {compounds_sentence}")
             button = st.button('Check Carcinogenicity', on_click=self.predict_carc, args=(st.session_state.data,))
+    
+        # Function to get combined embedding
+    def get_combined_embedding(self, text):
+        
+        inputs = self.tokenizer_pubmedbert(text, return_tensors="pt", truncation=True, max_length=512)
+        with torch.no_grad():
+            outputs = self.model_pubmedbert(**inputs)
+        cls_embedding = outputs.last_hidden_state[0, 0].numpy()
+        return cls_embedding
+
 
     def predict_carc(self, compound_list):
-        pred = self.svm_model.predict(compound_list)
+        embeddings = self.get_combined_embedding(compound_list)
+        pred = self.svc_model.predict(embeddings)
         if pred == 0:
             prediction = 'HIGH'
         elif pred == 1:
